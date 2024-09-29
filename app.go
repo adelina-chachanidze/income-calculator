9d5f3fe8496065ccecec2e3ca4aa88f8445c659a
Adelina <154084420+adelina-chachanidze@users.noreply.github.com>
2024-09-29 18:27:13 +0300

    Create app.go

---
+++ app.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Enter revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Enter expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Enter your tax rate: ")
+	fmt.Scan(&taxRate)
+
+	var beforeTax = revenue - expenses
+	var afterTax = beforeTax - (beforeTax * (taxRate / 100))
+	var ratio = beforeTax / afterTax
+
+	fmt.Println("Your EBT:", beforeTax)
+	fmt.Println("Your income:", afterTax)
+	fmt.Println("Your ratio:", ratio)
+}
+
