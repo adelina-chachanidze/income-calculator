4c96c6cf9065e485e3d9eedd62c75333c242cf34
Adelina <154084420+adelina-chachanidze@users.noreply.github.com>
2024-10-04 20:44:25 +0300

    Update app.go

--- app.go
+++ app.go
@@ -3,29 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	//Classic version 
-	//var revenue float64
-	//var expenses float64
-	//var taxRate float64
+	Classic version 
+	var revenue float64
+	var expenses float64
+	var taxRate float64
 
-	//fmt.Print("Enter revenue: ")
-	//fmt.Scan(&revenue)
+	fmt.Print("Enter revenue: ")
+	fmt.Scan(&revenue)
 
-	//fmt.Print("Enter expenses: ")
-	//fmt.Scan(&expenses)
+	fmt.Print("Enter expenses: ")
+	fmt.Scan(&expenses)
 
-	//fmt.Print("Enter your tax rate: ")
-	//fmt.Scan(&taxRate)
+	fmt.Print("Enter your tax rate: ")
+	fmt.Scan(&taxRate)
 
-	//fmt.Print("Enter expenses: ")
-	//fmt.Scan(&expenses)
+	fmt.Print("Enter expenses: ")
+	fmt.Scan(&expenses)
 
-	//fmt.Print("Enter your tax rate: ")
-	//fmt.Scan(&taxRate)
+	fmt.Print("Enter your tax rate: ")
+	fmt.Scan(&taxRate)
 
-	//var beforeTax = revenue - expenses
-	//var afterTax = beforeTax - (beforeTax * (taxRate / 100))
-	//var ratio = beforeTax / afterTax
+	var beforeTax = revenue - expenses
+	var afterTax = beforeTax - (beforeTax * (taxRate / 100))
+	var ratio = beforeTax / afterTax
 
 	//With funcs version
 
